b3fcab7e131ddc2e8bf7ba153976b1ffb8e13a0b
Tiago Sullevan <devsullevan@gmail.com>
2021-08-22 13:39:48 -0300

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singleton - Start\n");
+
+            Console.WriteLine(Singleton.Instance.Value);
+            Singleton.Instance.Value++;
+            Console.WriteLine(Singleton.Instance.Value);
+
+            Console.WriteLine("\nSingleton - End");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,13 @@
+﻿namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton INSTANCE = new Singleton();
+
+        public int Value = 0;
+
+        private Singleton() { }
+
+        public static Singleton Instance { get { return INSTANCE; } }
+    }
+}
